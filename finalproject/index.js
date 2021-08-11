839fe302499ea43c50d497f2ecf8bb419c09f11d
simon <s.leblanc1129@gmail.com>
2021-08-11 16:23:03 -0700

    resolved modal closing issue, added increment/decrement counters buttons for product quantity and added total to modal

--- finalproject/index.js
+++ finalproject/index.js
@@ -1,12 +1,55 @@
+
 //define initial variables
-const modal = document.getElementById("modal");
+let modal = document.getElementById("modal");
 
-//declare cart starting quantity
-let cart = 0
+let openModalButton = document.getElementById('cartModal');
 
-//modal display controls
-function displayModal() {
+let closeModalButton = document.getElementById('closeModal');
 
-    //allow modal to display
+let addtoCartButton = document.getElementById('cartAddButtonContainer');
+
+let cartQuantity = 0
+
+document.getElementById("quantity").innerText = cartQuantity;
+
+//modal window display
+//open modal if cart area is clicked
+openModalButton.onclick = function() {
     modal.style.display = "block";
-}
+};
+
+closeModalButton.onclick = function() {
+    modal.style.display = "none";
+};
+
+
+//quantity tracking
+
+function increaseQuantity () {
+
+    //implement function for incrementing quantity up each time add to cart button is clicked
+    cartQuantity = cartQuantity + 1;
+    document.getElementById("quantity").innerText = cartQuantity;
+};
+
+function decreaseQuantity () {
+
+    //implement function for incrementing quantity up each time add to cart button is clicked
+    cartQuantity = cartQuantity - 1;
+    document.getElementById("quantity").innerText = cartQuantity;
+};
+    
+    // implement function for incrementing quantity up each time increase button is clicked
+    //store value in cartQuantity array
+
+
+    //implement function for incrementing quantity down each time decrease button is clicked
+    //store value in cartQuantity array
+
+
+    //display cartQuantity value in modal
+
+
+
+
+
