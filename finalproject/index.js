1c74d79862a0bb225b37ab91413b2027233c52d6
simon <s.leblanc1129@gmail.com>
2021-08-10 18:55:13 -0700

    completed non-modal html/css layout and begun work on modal display

---
+++ finalproject/index.js
@@ -0,0 +1,12 @@
+//define initial variables
+const modal = document.getElementById("modal");
+
+//declare cart starting quantity
+let cart = 0
+
+//modal display controls
+function displayModal() {
+
+    //allow modal to display
+    modal.style.display = "block";
+}
